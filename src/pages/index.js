f289371e416e5935f6357a4ca7189b304117f5ba
Prashanth Kuchanpally <prshnth@users.noreply.github.com>
2020-05-19 22:49:10 -0400

    importing correct css

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,10 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import AboutMeCard from "../components/aboutMeCard"
-import "@fortawesome/free-solid-svg-icons"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"
+// Prevent fontawesome from dynamically adding its css since we did it manually above
+config.autoAddCss = false
 
 const IndexPage = () => (
   <Layout>
