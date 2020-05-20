b2bdc0e9656ae4de6193f58ebe45bd7d70caf40e
Prashanth Kuchanpally <prshnth@users.noreply.github.com>
2020-05-19 22:40:31 -0400

    loading fontawesome icons initially

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import AboutMeCard from "../components/aboutMeCard"
+import "@fortawesome/free-solid-svg-icons"
 
 const IndexPage = () => (
   <Layout>
